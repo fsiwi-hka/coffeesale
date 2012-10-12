05808375ced208a13808e2b02fcb2fa3dfc0c95a
Mathias Garbe <me@mathias-garbe.de>
2012-10-12 18:18:32 +0200

    Adds unfinished admin window

--- rebuildUi.sh
+++ rebuildUi.sh
@@ -3,8 +3,10 @@ rm codeUi.py
 rm messageUi.py
 rm tosUi.py
 rm screensaverUi.py
+rm adminUi.py
 pyuic4 main.ui -o mainUi.py
 pyuic4 code.ui -o codeUi.py
 pyuic4 message.ui -o messageUi.py
 pyuic4 tos.ui -o tosUi.py
 pyuic4 screensaver.ui -o screensaverUi.py
+pyuic4 admin.ui -o adminUi.py

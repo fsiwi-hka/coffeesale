98157dc1b9b3760e339c04ca6015b1e56af65c28
Mathias Garbe <me@mathias-garbe.de>
2013-02-08 13:56:48 +0100

    Moves Ui files to Ui/

---
+++ Ui/rebuildUi.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+pyuic4 main.ui -o mainUi.py
+pyuic4 code.ui -o codeUi.py
+pyuic4 message.ui -o messageUi.py
+pyuic4 tos.ui -o tosUi.py
+pyuic4 screensaver.ui -o screensaverUi.py
+pyuic4 admin.ui -o adminUi.py
+pyuic4 adminItem.ui -o adminItemUi.py
+pyuic4 adminStats.ui -o adminStatsUi.py
